4e91490cc828f5f4ab7806fe9638a02e79927eba
Melvyn <melvyn.goualard@gmail.com>
2026-01-19 15:19:37 +0100

    style: reformater le code pour améliorer la lisibilité dans activities.controller.ts

--- apps/api/src/relation-types/relation-types.controller.ts
+++ apps/api/src/relation-types/relation-types.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { RelationTypesService } from './relation-types.service';
 import { CreateRelationTypeDto } from './dto/create-relation-type.dto';
 import { UpdateRelationTypeDto } from './dto/update-relation-type.dto';
@@ -23,7 +31,10 @@ export class RelationTypesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRelationTypeDto: UpdateRelationTypeDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateRelationTypeDto: UpdateRelationTypeDto,
+  ) {
     return this.relationTypesService.update(+id, updateRelationTypeDto);
   }
 
