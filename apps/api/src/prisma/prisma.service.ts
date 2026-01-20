08a5f7379ffa79bdf3915208da60671b94d5e5a6
Melvyn <melvyn.goualard@gmail.com>
2026-01-20 10:42:04 +0100

    fix: Corriger l'importation de PrismaClient pour utiliser le chemin généré

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client';
 import 'dotenv/config';
+import { PrismaClient } from '../../prisma/generated/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
